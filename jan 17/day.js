d03ae9701b88111239e753b99bcb71492627cace
sanidhya <ayushbpl52@gmail.com>
2025-01-17 11:54:25 +0530

    file

---
+++ jan 17/day.js
@@ -0,0 +1,31 @@
+
+// function akash(){
+//     let ans1= parseFloat(prompt("enter number 1"))
+//     let ans2= parseFloat(prompt("enter number 2"))
+
+//     let sum=ans1+ans2;
+//     alert(ans1+ans2)
+
+// }
+
+// function sanidhya(){
+//    let greet=prompt("enter number")
+//    if(greet%2==0){
+//     alert("even")
+
+//    }
+//    else{
+//     alert("odd")
+//    }
+
+// }
+function sanidhya(){
+    let sm=prompt("enter a age")
+    if (sm>=18) {
+        
+      alert("18+") 
+    } else {
+        alert("under 18")
+       
+    }
+}
