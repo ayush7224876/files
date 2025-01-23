c4adab354c6b9afc7ec99bd5036cf59f692613eb
sanidhya <ayushbpl52@gmail.com>
2025-01-23 12:06:22 +0530

    js

---
+++ jan 23/array.js
@@ -0,0 +1,9 @@
+// let fruits=["apple","manga","banana","papya"];
+// fruits.splice(1,2,"orange")
+// document.write(fruits.slice(2,4))
+// // document.write(fruits)
+// fruits.reverse()
+// document.write(fruits)
+
+let abc=[2,3,4,5,7,8,9,1]
+document.write(abc.sort().reverse())
