9d1171463d2a111ceac41aa67e4c4964d645ba2f
sanidhya <ayushbpl52@gmail.com>
2025-02-03 10:47:37 +0530

    js

---
+++ jan 31/js.js
@@ -0,0 +1,24 @@
+// let student ={
+//     name: "sanidhya ",
+//     age: 24,
+
+// };
+// var butoon =document.querySelector("button")
+
+
+// let {name,age}=student
+// bt1.addEventListener("click ", function () {
+//     document.write(name,age)
+// })
+
+
+let st1={
+    name: "sanidhya", age:24,
+
+};
+let st2={
+    city: "bhopal", gali:bhenkada,
+};
+let combinedstudent={...st1,...st2};
+        console.lo(combinedstudent);
+
