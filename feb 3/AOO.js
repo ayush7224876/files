a99ba3f176d9c7f4bb44c23fe49a18643c0f0a99
sanidhya <ayushbpl52@gmail.com>
2025-02-04 10:56:20 +0530

    js

---
+++ feb 3/AOO.js
@@ -0,0 +1,44 @@
+// let arr=()=>{
+//     console.log("arrow fun");
+// }
+// arr()
+    
+
+// let text = "this is string"
+// let okk=[2,3,4,5,6,7]
+// let obb={name:"sandy",age:24}
+
+// let Aoo =[{
+// name:"sandy",
+// age:"24",
+// city:"chandigarh"
+// },{
+//     name:"sany",
+// age:"29",
+// city:"up"
+    
+// },{
+//     name:"ndy",
+// age:"19",
+// city:"indore"
+// },{
+//     name:"andy",
+// age:"21",
+// city:"bhopal"
+// }
+
+// ]
+
+
+
+//map, filter, forEach
+
+
+let arr1=[3,4,5,6,7,8,9,]
+let ans= arr1.map((e)=> e*2)
+console.log(ans);
+
+let ans2= arr1.filter((e)=> {
+    return e%2!=0
+})
+console.log( ans2);
